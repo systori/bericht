5894fa979ad3d3c4b034e0fcc729fb59a0e4bfef
Lex Berezhny <lex@damoti.com>
2017-09-04 21:37:39 -0400

    0.1.01

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ long_description = '\n\n'.join((
 
 setup(
     name='bericht',
-    version='0.0.14',
+    version='0.1.01',
     url='https://github.com/systori/bericht',
     license='BSD',
     description='Improved tabular report generation with ReportLab.',
