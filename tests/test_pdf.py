d8e3e54455a221e71e7788ac977ae51383212b7c
Lex Berezhny <lex@damoti.com>
2017-09-28 17:01:51 -0400

    wip tests

--- tests/test_pdf.py
+++ tests/test_pdf.py
@@ -3,22 +3,39 @@ from bericht.pdf import PDFStreamer
 
 
 def html_gen():
-    yield '<p>first ' + ' some really long sentance' * 0
+    yield '<p>begin hello world<br>'
     for i in range(1, 3):
-        yield 'hello world {}</p><p>'.format(i)
-    yield 'end</p>'
+        yield '{} hello world<br>'.format(i)
+    yield 'end hello world</p>'
+    yield '<p>new paragraph</p>'
+    yield """<table>
+  <colgroup>
+    <col width="1*" />
+    <col width="0*">April 6, 2017</col>
+  </colgroup>
+  <tr>
+    <td><p>Angebot (2.1)</p></td>
+    <td><p>April 6, 2017</p></td>
+  </tr>
+</table>"""
+    yield '<p>now for big table</p>'
+    yield '<p>now for big table</p>'
+    yield '<p>now for big table</p>'
     yield '<table>'
     yield '<colgroup>'
     yield '<col width="0*">row 99 col 99</col>'
-    yield '<col width="3*"></col>'
-    yield '<col width="1*"></col>'
+    yield '<col width="3*">'
+    yield '<col width="1*">'
     yield '</colgroup>'
     yield '<thead>'
-    yield '<tr><td><p>name</td><td><p>description</td><td><p>row</td>'
-    yield '<tr><td></td><td><p>multi row header</td><td></td>'
+    yield '<tr><td><p>name</td><td><p>description</td><td><p>row</td></tr>'
+    yield '<tr><td></td><td colspan="2"><p>multi row header</td></tr>'
     yield '</thead>'
+    #yield '<tfoot>'
+    #yield '<tr><td colspan="3"><p>the footer</td></tr>'
+    #yield '</tfoot>'
     for i in range(1, 30):
-        yield '<tr><td><p>row {} col 1</p></td><td><p>row {} col 2 {}</p></td><td><p>{}</p></td></tr>'.format(i, i, 'blah blah' * 10, i)
+        yield '<tr><td><p>row {} col 1</p></td><td><p>row {} col 2 {}</p></td><td><p>{}</p></td></tr>'.format(i, i, 'blah blah' * 11, i)
     yield '</table>'
     yield '<p>last p</p>'
 
--- tests/test_text.py
+++ tests/test_text.py
@@ -2,11 +2,11 @@ from unittest import TestCase
 
 from reportlab.pdfgen.canvas import Canvas
 
-from bericht.text import *
-from bericht.style import Style
+from bericht.html import HTMLParser
+from bericht.node import Paragraph, Break
 
 
-class BasePDFTestCase(TestCase):
+class BaseTestCase(TestCase):
 
     def draw(self, p):
         canvas = Canvas(None)
@@ -18,39 +18,85 @@ class BasePDFTestCase(TestCase):
     def assertPDF(self, input, output):
         self.assertEqual(self.draw(input), ' '.join(output))
 
+    @staticmethod
+    def parse(html):
+        return next(iter(HTMLParser(iter(html if isinstance(html, list) else [html]))))
 
-class TestText(BasePDFTestCase):
+
+class TestFormattingTagParsing(BaseTestCase):
+
+    def test_bold_italic_underline(self):
+
+        p = self.parse(
+            '<p><b>hello</b> <i>world</i>, <u>bericht</u>! '
+            '<strong>strong</strong> <b><i><u>biu</u></i></b></p>'
+        )
+
+        self.assertIsInstance(p, Paragraph)
+
+        words = p.words
+        self.assertEqual(len(words), 5)
+
+        bold, italic, underline, strong, biu = (w.parts[0] for w in words)
+
+        self.assertEqual(bold.style.bold, True)
+        self.assertEqual(bold.style.italic, False)
+        self.assertEqual(bold.style.underline, False)
+
+        self.assertEqual(strong.style.bold, True)
+        self.assertEqual(strong.style.italic, False)
+        self.assertEqual(strong.style.underline, False)
+
+        self.assertEqual(italic.style.bold, False)
+        self.assertEqual(italic.style.italic, True)
+        self.assertEqual(italic.style.underline, False)
+
+        self.assertEqual(underline.style.bold, False)
+        self.assertEqual(underline.style.italic, False)
+        self.assertEqual(underline.style.underline, True)
+
+        self.assertEqual(biu.style.bold, True)
+        self.assertEqual(biu.style.italic, True)
+        self.assertEqual(biu.style.underline, True)
+
+    def test_chunking(self):
+        p = self.parse(["<p>", "I", "’ve ", "I’ve</p>"])
+        words = p.words
+        self.assertEqual(len(words), 2)
+        self.assertEqual(str(words[0]), 'I’ve')
+        self.assertEqual(str(words[1]), 'I’ve')
+
+    def test_nesting(self):
+        p = self.parse(["<p>", "I<b>", "’<i>ve</i> ", "I</b>’ve</p>"])
+        words = p.words
+        self.assertEqual(len(words), 2)
+        self.assertEqual(str(words[0]), 'I’ve')
+        self.assertEqual(str(words[1]), 'I’ve')
+
+    def test_br_tag(self):
+        p = self.parse("<p>Break tag<br/>should close word.</p>")
+        words = p.words
+        self.assertEqual(len(words), 6)
+        self.assertEqual(str(words[1]), 'tag')
+        self.assertEqual(words[2], Break)
+        self.assertEqual(str(words[3]), 'should')
+
+    def test_nbsp_ignored(self):
+        p = self.parse("<p>Non &nbsp; breaking space.</p>")
+        words = p.words
+        self.assertEqual(len(words), 3)
+        self.assertEqual(str(words[0]), 'Non')
+        self.assertEqual(str(words[1]), 'breaking')
+
+
+class TestRendering(BaseTestCase):
 
     def test_wrap(self):
-        text = static(
-            "Some people don't like change, but you need to "
-            "embrace change if the alternative is disaster."
+        p = self.parse(
+            "<p>Some people don't like change, but you need to "
+            "embrace change if the alternative is disaster.</p>"
         )
-        self.assertEqual(text.width, None)
-        self.assertEqual(text.text_width, 494.55600000000004)
-        self.assertEqual(text.min_content_width, 494.55600000000004)
-        self.assertEqual(text.height, 14)
-        self.assertEqual(text.wrapOn(None, 20, 10), (494.55600000000004, 14))
-        self.assertEqual(text.width, 20)
-
-    def test_draw(self):
-        self.assertPDF(static('hello world!'), [
-            "BT",
-            "1 0 0 1 0 0 Tm",
-            "/F1 12 Tf",
-            "(hello world!) Tj",
-            "ET"
-        ])
-
-
-class TestParagraph(BasePDFTestCase):
-
-    def test_wrap(self):
-        text = para(
-            "Some people don't like change, but you need to "
-            "embrace change if the alternative is disaster."
-        )
-        self.assertEqual(text.wrap(100), (100, 84))
+        self.assertEqual(p.wrap(100), (100, 84))
 
     def test_draw_merges_same_styled_words(self):
         s = Style.default()
